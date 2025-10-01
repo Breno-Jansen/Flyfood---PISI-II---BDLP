d4f13c83c8233288351d14a4b7e66c68da20bafa
Pedro Peres <166012767+PePeres007@users.noreply.github.com>
2025-10-01 16:53:53 -0300

    Adicionando o arquivo da interface gráfica

---
+++ build/gui.py
@@ -0,0 +1,88 @@
+from pathlib import Path
+from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
+
+
+def relative_to_assets(path: str) -> Path:
+    output_path = Path(__file__).parent
+    assets_path = output_path / "assets" / "frame0"
+    return assets_path / Path(path)
+
+window = Tk()
+window.geometry("1221x672")
+window.configure(bg = "#409AE4")
+
+
+canvas = Canvas(
+    window,
+    bg = "#409AE4",
+    height = 672,
+    width = 1221,
+    bd = 0,
+    highlightthickness = 0,
+    relief = "ridge"
+)
+
+canvas.place(x = 0, y = 0)
+image_image_1 = PhotoImage(
+    file=relative_to_assets("image_1.png"))
+image_1 = canvas.create_image(
+    610.0,
+    336.0,
+    image=image_image_1
+)
+
+button_image_1 = PhotoImage(
+    file=relative_to_assets("button_1.png"))
+button_1 = Button(
+    image=button_image_1,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_1 clicked"),
+    relief="flat",
+    activebackground="#409AE4"
+)
+button_1.place(
+    x=196.0,
+    y=477.0,
+    width=356.0,
+    height=116.0
+)
+
+entry_image_1 = PhotoImage(
+    file=relative_to_assets("entry_1.png"))
+entry_bg_1 = canvas.create_image(
+    379.5,
+    352.5,
+    image=entry_image_1
+)
+entry_1 = Text(
+    bd=0,
+    bg="#EDEDED",
+    fg="#000716",
+    highlightthickness=0
+)
+entry_1.place(
+    x=205.0,
+    y=240.0,
+    width=349.0,
+    height=223.0
+)
+
+button_image_2 = PhotoImage(
+    file=relative_to_assets("button_2.png"))
+button_2 = Button(
+    image=button_image_2,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: print("button_2 clicked"),
+    relief="flat",
+    activebackground="#409AE4"
+)
+button_2.place(
+    x=696.0,
+    y=493.0,
+    width=356.0,
+    height=116.0
+)
+window.resizable(False, False)
+window.mainloop()
